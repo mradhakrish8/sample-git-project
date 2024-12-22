e453c84886a8d42d759635366d0b6d5b667ab8b6
mradhakrish8 <mradhakrish8@gmail.com>
2024-12-22 19:08:46 +0530

    Initial Commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,27 @@
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private double empSalaray;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSalaray() {
+		return empSalaray;
+	}
+	public void setEmpSalaray(double empSalaray) {
+		this.empSalaray = empSalaray;
+	}
+	
+	
+}
